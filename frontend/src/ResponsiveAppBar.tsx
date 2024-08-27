8e141aaf29e0d208c6e724912992b4c9bbaaecc1
meirad <94304926+meirad@users.noreply.github.com>
2024-08-27 20:24:21 +0300

    fix

--- frontend/src/ResponsiveAppBar.tsx
+++ frontend/src/ResponsiveAppBar.tsx
@@ -14,7 +14,7 @@ import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
 import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
 import { useContext, useState } from 'react';
-import {  ExpirationCardsContext } from './IconContext';
+import { ExpirationCardsContext } from './IconContext';
 import Popover from '@mui/material/Popover';
 import Badge from '@mui/material/Badge';
 import { styled } from '@mui/material/styles';
@@ -24,7 +24,6 @@ import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
 import TextField from '@mui/material/TextField';
 
-
 const pages = [""];
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
@@ -32,38 +31,30 @@ const RedDotBadge = styled(Badge)(({ theme }) => ({
   '& .MuiBadge-dot': {
     backgroundColor: 'red',
     color: 'red',
-      position: 'relative',
-      right: '24px',
-      top: '0px',
-      width: '10px',
-      height: '10px',
-  
+    position: 'relative',
+    right: '24px',
+    top: '0px',
+    width: '10px',
+    height: '10px',
   },
 }));
 
 function ResponsiveAppBar() {
   const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
   const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null);
-  const [open, setOpen] = useState(false);  // State for controlling the Popover
+  const [openPopover, setOpenPopover] = useState(false);
   const { expirationCards } = useContext(ExpirationCardsContext);
   const [notificationTime, setNotificationTime] = useState('');
-  const [openDialog, setOpenDialog] = React.useState(false);
-
-  console.log(expirationCards);
-  
-
+  const [openDialog, setOpenDialog] = useState(false);
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setNotificationTime(event.target.value);
   };
 
-
   const handleSaveClick = () => {
     localStorage.setItem('notificationTime', notificationTime);
     setOpenDialog(false);
   };
-
-
 
   const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorElNav(event.currentTarget);
@@ -81,25 +72,21 @@ function ResponsiveAppBar() {
     setAnchorElUser(null);
   };
 
-  const handleClickOpen = (event: React.MouseEvent<HTMLElement>) => {
-    setOpen(true);
+  const handleClickOpenPopover = () => {
+    setOpenPopover(true);
   };
 
-  const handleClose = () => {
-    setOpen(false);
-
+  const handleClosePopover = () => {
+    setOpenPopover(false);
   };
 
-  const OpenDialog = () => {
+  const handleOpenDialog = () => {
     setOpenDialog(true);
-  }
+  };
 
-
-  const CloseDialog = () => {
+  const handleCloseDialog = () => {
     setOpenDialog(false);
-  }
-
-
+  };
 
   return (
     <AppBar position="static">
@@ -160,6 +147,7 @@ function ResponsiveAppBar() {
               ))}
             </Menu>
           </Box>
+
           <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
           <Typography
             variant="h5"
@@ -179,6 +167,7 @@ function ResponsiveAppBar() {
           >
             LOGO
           </Typography>
+
           <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
             {pages.map((page) => (
               <Button
@@ -191,26 +180,24 @@ function ResponsiveAppBar() {
             ))}
           </Box>
 
-          <div>
-          <RedDotBadge
-            overlap="circular"
-            badgeContent=""
-            variant="dot"
-            anchorOrigin={{
-              vertical: 'top',
-              horizontal: 'right',
-            }}
-            style={{ cursor: 'pointer',  position: 'relative', zIndex: 100  }} 
-            onClick={handleClickOpen}
-            invisible={expirationCards.length === 0}
-          >
-            <NotificationsActiveIcon 
-              style={{ marginRight: '20px', cursor: 'pointer' }} 
-            />
-          </RedDotBadge>
+          <Box>
+            <RedDotBadge
+              overlap="circular"
+              variant="dot"
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              style={{ cursor: 'pointer', position: 'relative', zIndex: 100 }}
+              onClick={handleClickOpenPopover}
+              invisible={expirationCards.length === 0}
+            >
+              <NotificationsActiveIcon style={{ marginRight: '20px', cursor: 'pointer' }} />
+            </RedDotBadge>
+
             <Popover
-              open={open}
-              onClose={handleClose}
+              open={openPopover}
+              onClose={handleClosePopover}
               anchorOrigin={{
                 vertical: 'top',
                 horizontal: 'right',
@@ -228,37 +215,36 @@ function ResponsiveAppBar() {
                 </div>
               ))}
             </Popover>
-          </div>
+          </Box>
 
           <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton sx={{ p: 0 }} onClick={handleOpenDialog}>
+                <Avatar alt="User Avatar" src="/static/images/avatar/2.jpg" />
+              </IconButton>
+            </Tooltip>
 
-          <Tooltip title="Open settings">
-          <IconButton sx={{ p: 0 }} onClick={() => setOpenDialog(true)}>
-            <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-          </IconButton>
-        </Tooltip>
+            <Dialog open={openDialog} onClose={handleCloseDialog}>
+              <DialogTitle>Change Notification Lead Time</DialogTitle>
+              <DialogContent>
+                <TextField
+                  autoFocus
+                  margin="dense"
+                  id="notification-time"
+                  label="Notification Lead Time (only enter a number)"
+                  type="text"
+                  fullWidth
+                  variant="standard"
+                  value={notificationTime}
+                  onChange={handleInputChange}
+                />
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={handleCloseDialog}>Cancel</Button>
+                <Button onClick={handleSaveClick}>Save</Button>
+              </DialogActions>
+            </Dialog>
 
-
-          <Dialog open={openDialog} onClose={() => setOpenDialog(false)}>
-            <DialogTitle>Change Notification Lead Time</DialogTitle>
-            <DialogContent>
-              <TextField
-                autoFocus
-                margin="dense"
-                id="name"
-                label="Notification Lead Time (only enter a number)"
-                type="text"
-                fullWidth
-                variant="standard"
-                value={notificationTime}
-                onChange={handleInputChange}
-              />
-            </DialogContent>
-            <DialogActions>
-              <Button onClick={() => setOpenDialog(false)}>Cancel</Button>
-              <Button onClick={handleSaveClick}>Save</Button>
-            </DialogActions>
-          </Dialog>
             <Menu
               sx={{ mt: '45px' }}
               id="menu-appbar"
